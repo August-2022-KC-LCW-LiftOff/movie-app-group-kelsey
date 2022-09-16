7d902659dd1dccb84217ad9f0f5ba6bd5c3ca8e2
Lisa Ulricksen <lisa.joshua1.9@gmail.com>
2022-09-16 12:11:07 -0500

    working state

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,16 +9,24 @@ import { AddReviewComponent } from './add-review/add-review.component';
 import { FaqPageComponent } from './faq-page/faq-page.component';
 import { MovieListComponent } from './movie-list/movie-list.component';
 import { CarouselComponent } from './carousel/carousel.component';
-import { CommentsComponent } from './comments/comments.components';
-import { CommentsModule } from './comments/comments.module';
+//import { CommentsComponent } from './comments/comments.components';
+//import { CommentsModule } from './comments/comments.module';
 import { HttpClientModule } from '@angular/common/http';
 import { NavbarComponent } from './navbar/navbar.component';
 import { CommentFormComponent } from './commentForm/commentForm.component';
-import { CommentComponent } from './comment/comment.component';
+//import { CommentComponent } from './comment/comment.component';
 import { CommentsService } from './comments/comments.service';
 import { FormBuilder } from '@angular/forms';
 import { SignInPageComponent } from './sign-in-page/sign-in-page.component';
-import { CreateAnAccountComponent } from './create-an-account/create-an-account.component'
+import { CreateAnAccountComponent } from './create-an-account/create-an-account.component';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireMessagingModule } from '@angular/fire/compat/messaging';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { AngularFireFunctionsModule } from '@angular/fire/compat/functions';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -30,15 +38,22 @@ import { CreateAnAccountComponent } from './create-an-account/create-an-account.
     MovieListComponent,
     CarouselComponent,
     NavbarComponent,
-    CommentsComponent,
-    CommentFormComponent,
-    CommentComponent,
+    //CommentsComponent,
+    //CommentFormComponent,
+    //CommentComponent,
     SignInPageComponent,
     CreateAnAccountComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule,
+    AngularFireMessagingModule,
+    AngularFireDatabaseModule,
+    AngularFireFunctionsModule,
     AppRoutingModule, 
         RouterModule.forRoot([
       { path: '', component: HomeComponent },
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'triggered-f9073',
+    appId: '1:400237670922:web:1e656a16d4afede78725f3',
+    storageBucket: 'triggered-f9073.appspot.com',
+    apiKey: 'AIzaSyCo6rDxkNNTmLjYF1z4BXB3-NROxdfKfTU',
+    authDomain: 'triggered-f9073.firebaseapp.com',
+    messagingSenderId: '400237670922',
+    measurementId: 'G-WG2ZYR04SR',
+  },
   production: false,
 };
 
