837f6b1222303d0c430abf5e481d1c097effac9f
srh-golubski <srh.sexton@gmail.com>
2022-10-12 10:48:38 -0500

    please don't break

---
+++ src/app/drive-in-image/drive-in-image.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-drive-in-image',
+  templateUrl: './drive-in-image.component.html',
+  styleUrls: ['./drive-in-image.component.css']
+})
+export class DriveInImageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
